75e186dccd146007b2f04be257e640ec28c37ada
Filip Simko <flpmko@gmail.com>
2022-04-20 12:46:44 +0200

    fixed async issues and first load

--- app/components/song/SongsList.js
+++ app/components/song/SongsList.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useLayoutEffect, useState } from 'react';
+import React, { useEffect, useLayoutEffect, useState } from "react";
 import {
   StyleSheet,
   FlatList,
@@ -8,36 +8,37 @@ import {
   Text,
   TouchableOpacity,
   ActivityIndicator,
-} from 'react-native';
-import Popover from 'react-native-popover-view/dist/Popover';
-import { Ionicons } from '@expo/vector-icons';
-import { StatusBar } from 'expo-status-bar';
-import { filter, _ } from 'lodash';
-import { doc, getDoc } from 'firebase/firestore';
-import NetInfo from '@react-native-community/netinfo';
+  Alert,
+} from "react-native";
+import Popover from "react-native-popover-view/dist/Popover";
+import { Ionicons } from "@expo/vector-icons";
+import { StatusBar } from "expo-status-bar";
+import { filter, _ } from "lodash";
+import { doc, getDoc } from "firebase/firestore";
+import NetInfo from "@react-native-community/netinfo";
 
-import { db } from '../../../firebase-config';
-import { UserContext } from '../../util/UserManager';
+import { db } from "../../../firebase-config";
+import { UserContext } from "../../util/UserManager";
 import {
   getStoredData,
   getStoredObjectData,
   storeData,
   storeObjectData,
-} from '../../util/LocalStorage';
-import ListItem from '../list/ListItem';
-import Separator from '../list/Separator';
-import SearchBar from '../SearchBar';
-import SearchFilterBar from '../SearchFilterBar';
+} from "../../util/LocalStorage";
+import ListItem from "../list/ListItem";
+import Separator from "../list/Separator";
+import SearchBar from "../SearchBar";
+import SearchFilterBar from "../SearchFilterBar";
 
-import colors from '../../config/colors';
-import songs_data from '../../data/songs_data';
+import colors from "../../config/colors";
+import songs_data from "../../data/songs_data";
 
 const SongsList = ({ route, navigation }) => {
   // ignore firebase timer warning for android
-  LogBox.ignoreLogs(['Setting a timer']);
+  LogBox.ignoreLogs(["Setting a timer"]);
   const _console = _.clone(console);
   console.warn = (message) => {
-    if (message.indexOf('Setting a timer') <= -1) {
+    if (message.indexOf("Setting a timer") <= -1) {
       _console.warn(message);
     }
   };
@@ -48,16 +49,17 @@ const SongsList = ({ route, navigation }) => {
   const [showFilters, setShowFilters] = useState(route.params.filters);
   const [loading, setLoading] = useState(false);
   const [allSongs, setAllSongs] = useState(
-    route.params.filters ? songs_data : favorites
+    route.params.filters ? null : favorites
   );
   const [songs, setSongs] = useState(allSongs);
   const [hymns, setHymns] = useState([]);
   const [modern, setModern] = useState([]);
-  const [seasonQuery, setSeasonQuery] = useState('');
-  const [query, setQuery] = useState('');
+  const [seasonQuery, setSeasonQuery] = useState("");
+  const [query, setQuery] = useState("");
 
-  const hymnsRef = doc(db, 'index/hymns');
-  const songsRef = doc(db, 'index/songs');
+  const hymnsRef = doc(db, "index/hymns");
+  const songsRef = doc(db, "index/songs");
+  const timesRef = doc(db, "index/timestamps");
 
   const [isPopoverVisible, setisPopoverVisible] = useState(false);
 
@@ -67,7 +69,7 @@ const SongsList = ({ route, navigation }) => {
 
   const handleLibraryChange = (library) => {
     togglePopover();
-    if (library === 'songs') {
+    if (library === "songs") {
       setSongs(allSongs);
       setShowFilters(true);
     } else {
@@ -97,7 +99,7 @@ const SongsList = ({ route, navigation }) => {
 
   // filter the list of hymns based on season
   const handleFilter = (filterQuery) => {
-    if (filterQuery === '') {
+    if (filterQuery === "") {
       setSongs(allSongs);
     } else {
       const myData = [].concat(allSongs).filter(function (el) {
@@ -108,7 +110,44 @@ const SongsList = ({ route, navigation }) => {
   };
 
   const goToSong = (item) => {
-    navigation.push('SongDetail', { song: item });
+    navigation.push("SongDetail", { song: item });
+  };
+
+  const fetchFromDb = async () => {
+    const timesDoc = await getDoc(timesRef);
+    const timeHymnsDb = timesDoc.get("hymns").valueOf();
+    const timeSongsDb = timesDoc.get("songs").valueOf();
+    const timeHymnsLocal = await getStoredData("timeHymnsLocal");
+    const timeSongsLocal = await getStoredData("timeSongsLocal");
+
+    console.log("timeHymnsDb = ", timeHymnsDb);
+    console.log("timeSongsDb = ", timeSongsDb);
+    console.log("timeHymnsLocal = ", timeHymnsLocal);
+    console.log("timeSongsLocal = ", timeSongsLocal);
+
+    if (timeHymnsLocal !== timeHymnsDb || !seasons || !songs) {
+      await storeData("timeHymnsLocal", timeHymnsDb);
+      const hymnsDoc = await getDoc(hymnsRef);
+      const hymnsData = hymnsDoc
+        .get("all")
+        .sort((a, b) => a?.number - b?.number);
+      const seasonsData = hymnsDoc.get("seasons");
+      await storeObjectData("hymnsData", hymnsData);
+      await storeObjectData("seasons", seasonsData);
+      setAllSongs(hymnsData);
+      setSeasons(seasonsData);
+      setSongs(hymnsData);
+    }
+
+    if (timeSongsLocal !== timeSongsDb || !modern) {
+      await storeData("timeSongsLocal", timeSongsDb);
+      const songsDoc = await getDoc(songsRef);
+      const songsData = songsDoc
+        .get("all")
+        .sort((a, b) => (a.title > b.title ? 1 : -1));
+      await storeObjectData("modern", songsData);
+      setModern(songsData);
+    }
   };
 
   useEffect(() => {
@@ -116,53 +155,38 @@ const SongsList = ({ route, navigation }) => {
       setLoading(true);
       let locData, locDataMod;
       if (showFilters) {
-        locData = await getStoredObjectData('hymnsData');
-        locDataMod = await getStoredObjectData('modern');
+        locData = await getStoredObjectData("hymnsData");
+        locData?.sort((a, b) => a?.number - b?.number);
+        locDataMod = await getStoredObjectData("modern");
       } else {
         locData = favorites;
+        locData?.sort((a, b) => (a.title > b.title ? 1 : -1));
       }
-      locData.sort((a, b) => a?.number - b?.number);
       if (locData) {
         setAllSongs(locData);
         setSongs(locData);
       }
-      if (locDataMod) setModern(locDataMod);
+      if (locDataMod) {
+        locDataMod.sort((a, b) => (a.title > b.title ? 1 : -1));
+        setModern(locDataMod);
+      }
       if (showFilters) {
-        const netInfo = await NetInfo.fetch();
-        if (netInfo.isInternetReachable) {
-          const data = await getDoc(hymnsRef);
-          const songsDoc = await getDoc(songsRef);
-          const lastChangeDb = data.get('lastChange').valueOf();
-          const seasonsData = data.get('seasons');
-          const songsData = songsDoc.get('all');
-          const hymnsData = data
-            .get('all')
-            .sort((a, b) => a?.number - b?.number);
-          const lastChangeLocal = await getStoredData('lastChange');
-          if (lastChangeLocal) {
-            if (lastChangeLocal !== lastChangeDb) {
-              await storeData('lastChange', lastChangeDb);
-              await storeObjectData('hymnsData', hymnsData);
-              await storeObjectData('seasons', seasonsData);
-              await storeObjectData('modern', songsData);
-              setAllSongs(hymnsData);
-              setSeasons(seasonsData);
-              setSongs(hymnsData);
-              setModern(songsData);
+        NetInfo.fetch().then(async (isInternetReachable) => {
+          if (isInternetReachable) {
+            await fetchFromDb();
+          } else {
+            if (!locData) {
+              Alert.alert(
+                "Upozornenie",
+                "Pre prvé spustenie aplikácie je potrebné internetové pripojenie. Vypnite prosím aplikáciu, pripojte sa na internet a potom aplikáciu opäť spustite."
+              );
             }
           }
-          if (!modern) {
-            console.log('nejsu');
-            await storeObjectData('modern', songsData);
-            setModern(songsData);
-          }
-          if (!seasons) {
-            await storeObjectData('seasons', seasonsData);
-            setSeasons(seasonsData);
-          }
-        }
+          setLoading(false);
+        });
+      } else {
+        setLoading(false);
       }
-      setLoading(false);
     };
     setup();
   }, []);
@@ -183,9 +207,9 @@ const SongsList = ({ route, navigation }) => {
             from={
               <TouchableOpacity onPress={togglePopover}>
                 <Ionicons
-                  name={'book'}
+                  name={"book"}
                   size={28}
-                  color={theme === 'dark' ? colors.primarydark : colors.primary}
+                  color={theme === "dark" ? colors.primarydark : colors.primary}
                 />
               </TouchableOpacity>
             }
@@ -193,26 +217,26 @@ const SongsList = ({ route, navigation }) => {
             <View>
               <TouchableOpacity
                 style={styles.containerPopup}
-                onPress={() => handleLibraryChange('songs')}
+                onPress={() => handleLibraryChange("songs")}
               >
                 <Ionicons
-                  name={showFilters ? 'checkmark' : 'file-tray-full'}
+                  name={showFilters ? "checkmark" : "file-tray-full"}
                   size={28}
                   style={styles.iconPopup}
-                  color={theme === 'dark' ? colors.primarydark : colors.primary}
+                  color={theme === "dark" ? colors.primarydark : colors.primary}
                 />
                 <Text style={styles.textPopup}>Spevníkové</Text>
               </TouchableOpacity>
               <Separator />
               <TouchableOpacity
                 style={styles.containerPopup}
-                onPress={() => handleLibraryChange('modern')}
+                onPress={() => handleLibraryChange("modern")}
               >
                 <Ionicons
-                  name={showFilters ? 'file-tray-full' : 'checkmark'}
+                  name={showFilters ? "file-tray-full" : "checkmark"}
                   size={28}
                   style={styles.iconPopup}
-                  color={theme === 'dark' ? colors.primarydark : colors.primary}
+                  color={theme === "dark" ? colors.primarydark : colors.primary}
                 />
                 <Text style={styles.textPopup}>Mládežnícke</Text>
               </TouchableOpacity>
@@ -252,7 +276,7 @@ const SongsList = ({ route, navigation }) => {
           <ActivityIndicator size="large" color={colors.primary} />
         </View>
       )}
-      <StatusBar style={theme === 'dark' ? 'light' : 'dark'} />
+      <StatusBar style={theme === "dark" ? "light" : "dark"} />
     </SafeAreaView>
   );
 };
@@ -265,11 +289,11 @@ const styles = StyleSheet.create({
   },
   containerSpinner: {
     flex: 1,
-    justifyContent: 'center',
+    justifyContent: "center",
   },
   horizontal: {
-    flexDirection: 'row',
-    justifyContent: 'space-around',
+    flexDirection: "row",
+    justifyContent: "space-around",
     padding: 10,
   },
   containerlight: {
@@ -280,21 +304,21 @@ const styles = StyleSheet.create({
   },
   containerSearch: {
     flex: 1,
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
     padding: 10,
   },
   containerAddButton: {
-    width: 'auto',
-    display: 'flex',
-    flexDirection: 'row',
+    width: "auto",
+    display: "flex",
+    flexDirection: "row",
   },
   containerPopup: {
-    display: 'flex',
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'flex-start',
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "flex-start",
     paddingVertical: 10,
   },
   iconPopup: {
@@ -318,7 +342,7 @@ const styles = StyleSheet.create({
     borderColor: colors.primary,
     borderWidth: 1,
     borderRadius: 15,
-    width: '98%',
+    width: "98%",
     fontSize: 18,
   },
   textdark: {
